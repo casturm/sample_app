29ce3173f4aeeb9ebf63329b969fa2f90ea5a7a1
Chris Sturm <casturm@gmail.com>
2013-05-21 12:17:19 -0700

    updates for new work laptop
     * update database config for new local development db
     * bump jquery version
     * commented out database cleaner .. not sure why, but tests pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,15 +31,15 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    #DatabaseCleaner.strategy = :transaction
+    #DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
-    DatabaseCleaner.start
+    #DatabaseCleaner.start
   end
 
   config.after(:each) do
-    DatabaseCleaner.clean
+    #DatabaseCleaner.clean
   end
 end
