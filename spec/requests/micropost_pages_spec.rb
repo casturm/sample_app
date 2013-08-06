ad5dd4106013cba99f016b3f9cfef183aedf797f
Chris Sturm <casturm@gmail.com>
2013-08-06 16:55:45 -0700

    10.3.4 Destroying microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -30,4 +30,18 @@ describe "MicropostPages" do
       end
     end
   end
+
+  describe "micropost destruction" do 
+    before do
+      FactoryGirl.create(:micropost, user: user)
+    end
+
+    describe "as current user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
